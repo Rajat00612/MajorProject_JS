eadef5c67e058299d5641609430969b0deba2d95
Rajat <rajatkumar17141@gmail.com>
2025-04-18 15:22:11 +0530

    Update mergepdf.js
    Fix code

--- Major_Project_JS 1 PDFX Merger/mergepdf.js
+++ Major_Project_JS 1 PDFX Merger/mergepdf.js
@@ -1,7 +1,8 @@
+
 const PDFMerger = require('pdf-merger-js');
 
 var merger = new PDFMerger();
-
+const outputPath = path.join(__dirname, 'uploads', `${filename}.pdf`);
 const mergePdfs = async (p1, p2) => {
   await merger.add(p1);  //merge all pages. parameter is the path to file and filename.
   await merger.add(p2); // merge only page 2 
@@ -11,6 +12,7 @@ const mergePdfs = async (p1, p2) => {
   // Export the merged PDF as a nodejs Buffer
   // const mergedPdfBuffer = await merger.saveAsBuffer();
   // fs.writeSync('merged.pdf', mergedPdfBuffer);
+return `${filename}`;  // Not full path
 } 
 
 module.exports = {mergePdfs}
